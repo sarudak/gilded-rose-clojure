ebf862a20ce8cf1d92104e030de9e14260dbd097
Sarudak <SPaulson@paraport.com>
2015-04-06 12:24:21 -0700

    Refactored the core code

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -1,36 +1,54 @@
 (ns gilded-rose.core)
 
+(def item-types {"Backstage passes to a TAFKAL80ETC concert" :ticket
+                 "Aged Brie" :brie
+                 "Sulfuras, Hand of Ragnaros" :sulfuras})
+
+(defn expired? [{sell-in :sell-in}] (< sell-in 0))
+(def increases-by-1 inc)
+(def increases-by-2 (comp inc inc))
+(def increases-by-3 (comp inc inc inc))
+(def decreases-by-1 dec)
+(def decreases-by-2 (comp dec dec))
+(def becomes-0 (fn [_] 0))
+
+(defn quality-of [item modifier] (update-in item [:quality] modifier))
+
+(defn get-item-type [item] (item-types (:name item)))
+(defmulti update-item-quality get-item-type)
+
+(defmethod update-item-quality :brie [item]
+  (if (expired? item)
+    (quality-of item increases-by-2)
+    (quality-of item increases-by-1)))
+
+(defmethod update-item-quality :ticket [{sell-in :sell-in :as item}]
+  (quality-of item (cond (>= sell-in 10) increases-by-1
+                         (>= sell-in 5) increases-by-2
+                         (>= sell-in 0) increases-by-3
+                         :else becomes-0)))
+
+(defmethod update-item-quality :default [item]
+  (if (expired? item)
+    (quality-of item decreases-by-2)
+    (quality-of item decreases-by-1)))
+
+(defn update-sellin [item]
+  (update-in item [:sell-in] decreases-by-1))
+
+(defn ensure-quality-bounds [item]
+  (update-in item [:quality] #(max 0 (min % 50))))
+
+(defmulti update-item get-item-type)
+(defmethod update-item :sulfuras [item] item)
+(defmethod update-item :default [item]
+  (-> item
+      update-sellin
+      update-item-quality
+      ensure-quality-bounds))
+
 (defn update-quality [items]
-  (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (min 50 (inc (inc (:quality item))))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (min 50 (inc (inc (inc (:quality item)))))})
-            (if (and (= (:name item) "Aged Brie") (< (:sell-in item) 0) (< (:quality item) 49))
-              (update-in item [:quality] (comp inc inc))
-              (if (< (:quality item) 50)
-                (merge item {:quality (inc (:quality item))})
-                item))))
-      (< (:sell-in item) 0)
-        (if (or (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-                (and (not= "Sulfuras, Hand of Ragnaros" (:name item))
-                     (< (:quality item) 3)))
-          (merge item {:quality 0})
-          (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-            (merge item {:quality (- (:quality item) 2)})
-            item))
-      (and (not= "Sulfuras, Hand of Ragnaros" (:name item)) (> (:quality item) 0))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+  (map update-item items))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
